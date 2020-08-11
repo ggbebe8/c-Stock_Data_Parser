24074cc30870c59223a38444efdc04c806f72f94
chops_pemtron <ggbebe7@gmail.com>
2020-08-11 13:11:07 +0900

    ÇÁ·ÎÁ§Ʈ »ý¼º

---
+++ StockDataParser/ParserMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace StockDataParser
+{
+    partial class ParserMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+       #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnParsing = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnParsing
+            // 
+            this.btnParsing.Location = new System.Drawing.Point(672, 358);
+            this.btnParsing.Name = "btnParsing";
+            this.btnParsing.Size = new System.Drawing.Size(75, 23);
+            this.btnParsing.TabIndex = 0;
+            this.btnParsing.Text = "Go";
+            this.btnParsing.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 290);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // ParserMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnParsing);
+            this.Name = "ParserMain";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParsing;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
