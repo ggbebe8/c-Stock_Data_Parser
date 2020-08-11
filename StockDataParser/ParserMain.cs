8c9f5bfcdff871e76d10302ab4e893935eb71bc8
chops_pemtron <ggbebe7@gmail.com>
2020-08-11 13:11:07 +0900

    Create Project

---
+++ StockDataParser/ParserMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockDataParser
+{
+    public partial class ParserMain : Form
+    {
+        public ParserMain()
+        {
+            InitializeComponent();
+        }
+
+        private void SetInit()
+        {
+            btnParsing.Click += BtnParsing_Click;
+        }
+
+        private void BtnParsing_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ StockDataParser/Variables.cs
@@ -0,0 +1,20 @@
+﻿namespace StockDataParser
+{
+    static class Variables
+    {
+        public struct DetailInfo
+        {
+            public string date;
+            public int price;
+            public int volumn;
+            public int highPrice;
+            public int lowPrice;
+            public int startPrice;
+        }
+
+        public static string DBROUTE = @"Data Source = .\Contents.db";
+
+        public static bool ISTIMER = true;
+
+    }
+}
